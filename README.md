0f97019a9ef9b0de6045907cb20107a78ae6b90a
jameswhf <jameswhf@qq.com>
2025-05-23 12:05:19 +0800

    enha 优化 java 代码执行命令

--- README.md
+++ README.md
@@ -19,6 +19,10 @@
 
 [注] 文件或文件夹路径包含 `/src/main/resources/` 时, 才会在右键中支持 `taskflow: 执行测试用例`
 
+### Yaml文件中 clazz 的预览和跳转
+
+![image](https://raw.githubusercontent.com/jameswhf/taskflow-yaml-run/main/src/assets/clazz_hover.png)
+
 ## Requirements
 API 自动化项目需要符合 `guandata taskflow` 的测试要求。
 
--- src/extension.js
+++ src/extension.js
@@ -3,6 +3,7 @@
 const vscode = require('vscode');
 const path = require('path');
 const fs = require('fs');
+const { execSync } = require('child_process');
 
 function findExistJavaFile (javaClassName) {
 	const javaClassPath = javaClassName.replace(/\./g, '/') + '.java';
@@ -37,6 +38,18 @@ function getJavaFilePreview (javaFilePath) {
 	}
 }
 
+function getMavenClasspath(projectDir) {
+    try {
+        // 在 Mac/Linux 下执行
+        const cmd = `cd "${projectDir}" && mvn dependency:build-classpath | grep -A 1 "Dependencies classpath:" | tail -n 1`;
+        const output = execSync(cmd, { encoding: 'utf-8' });
+        return output.trim(); // 返回 classpath 字符串
+    } catch (err) {
+        vscode.window.showErrorMessage('获取 Maven classpath 失败: ' + err.message);
+        return '';
+    }
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
@@ -49,18 +62,40 @@ function activate(context) {
 	console.log('Congratulations, your extension "taskflow-yaml-run" is now active!');
 
 	// 运行任务
-	async function runTaskflowJob(editor, filePath) {		
-		const command = `./mvnw clean compile exec:java -Dexec.mainClass="com.guandata.test.TestExecutor" -Dexec.args="-f ${filePath}" && open report/result_report.html`;
+	async function runTaskflowJob(filePath) {
 		try {
-			const terminal = vscode.window.terminals.find(t => t.name === 'Taskflow Run') || vscode.window.createTerminal('Taskflow Run');
-			terminal.sendText(command);
-			terminal.show();
+			// Get the current workspace folder
+			const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
+			if (!workspaceFolder) {
+				vscode.window.showErrorMessage('No workspace folder found. Please open a project.');
+				return;
+			}
+			const projectDir = workspaceFolder.uri.fsPath;
+
+			// 动态读取 classpath
+			const jarClassPathStr = getMavenClasspath(projectDir)
+			const projectClassPath = path.join(projectDir, 'target/classes')
+			// 生成 argfile
+			const executeArgFileStr = `-XX:+ShowCodeDetailsInExceptionMessages -cp "${projectClassPath}:${jarClassPathStr}"`
+			const argfilePath = path.join(projectDir, './.run/auto_gen_yaml_run.argfile')
+			fs.writeFileSync(argfilePath, executeArgFileStr, 'utf8');
+
+			// 运行 Java 代码，把当前的 yaml 文件作为参数传入
+			const runJavaCommand = `java @${argfilePath} com.guandata.test.TestExecutor -f ${filePath}`;
+			const openReportHtmlCommand = `open ${path.join(projectDir, './report/result_report.html')}`;
+			const command = `${runJavaCommand} && ${openReportHtmlCommand}`;
+			try {
+				const terminal = vscode.window.terminals.find(t => t.name === 'Taskflow Run') || vscode.window.createTerminal('Taskflow Run');
+				terminal.sendText(command);
+				terminal.show();
+			} catch (error) {
+				vscode.window.showErrorMessage(`Failed to run taskflow job: ${error.message}`);
+			}
 		} catch (error) {
 			vscode.window.showErrorMessage(`Failed to run taskflow job: ${error.message}`);
 		}
 	}
-	// 注册命令
-	const commandDisposable = vscode.commands.registerCommand('taskflow.runYamlJob', (fileOrDirPath) => {
+	function onExecuteYamlJob (fileOrDirPath, isDebug) {
 		const editor = vscode.window.activeTextEditor;
 		const currentWorkspacePath = vscode.workspace.workspaceFolders[0].uri.fsPath;
 		const yamlFileBaseDirPath = path.join(currentWorkspacePath, './src/main/resources')
@@ -70,13 +105,19 @@ function activate(context) {
 			const relativeYamlFilePath = currentYamlFilePath.slice(yamlFileBaseDirPath.length + 1)
 			if (relativeYamlFilePath) {
 				console.log('需要执行的yaml文件路径', relativeYamlFilePath)
-				runTaskflowJob(editor, relativeYamlFilePath);
+				runTaskflowJob(relativeYamlFilePath, isDebug);
 				return
 			}
 		}
 		vscode.window.showErrorMessage(`目标文件或文件夹的路径 ${currentYamlFilePath} 不符合运行规范, 路径中必须包含 /src/main/resources/ 目录`);
-	});
-	context.subscriptions.push(commandDisposable)
+	}
+
+	// 注册 run 命令
+    context.subscriptions.push(
+        vscode.commands.registerCommand('taskflow.runYamlJob', (fileOrDirPath) => {
+            onExecuteYamlJob(fileOrDirPath, false);
+        })
+    );
 
 	// 注册 yaml 中 clazz 识别和跳转
     const hoverProvider = vscode.languages.registerHoverProvider('yaml', {
